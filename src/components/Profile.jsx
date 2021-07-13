a9bdd8cfeba9ef2ea81ea7e18f0ef56763dd62e3
Ellie-Lissner <eswalker0723@gmail.com>
2021-07-13 12:01:38 -0500

    pushed branch

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -50,7 +50,6 @@ export default function Profile(props) {
 
                 <p>{message}</p>
                 
-                <Weather />
                 <Cal />
             </div>
         </div>
