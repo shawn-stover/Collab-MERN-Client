0089291052a9f26a2a8cbb2c9035da6613512ce8
nesbita <ariananesbit@gmail.com>
2021-07-13 15:25:31 -0600

    something small

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react"
 import { Redirect, Link } from "react-router-dom"
 import axios from 'axios'
 import Login from './Login'
+import Cal from './calendar/Cal'
 import CalendarView from './calendar/CalendarView'
 import Weather from './Weather'
-import styled from "styled-components";
 
 
 
@@ -46,44 +46,14 @@ export default function Profile(props) {
             <Weather />
             <h4>Greetings! {props.currentUser.name}👋</h4>
             <Link to="/calendar">Calendar</Link>
-            <h5>your email is {props.currentUser.email}</h5>
+            {/* <h5>your email is {props.currentUser.email}</h5>
 
             <div>
             <p>You have a secret message from the authorized user area!</p>
 
                 <p>{message}</p> 
                 
-
-             </div>
-
-                
-                <Events>
-                <Link to="/calendar">Calendar</Link>
-              <h2>Today's Schedule:</h2>
-              <h5><span>10:00am </span>- Brunch with Bill</h5>
-              <h5><span>12:00pm</span> - Lunch with Jill</h5>
-              <h5><span>2:00pm</span> - Meeting with Brian</h5>
-              <h5><span>5:00pm</span> - Pickup Jenny</h5>
-            </Events>
-                
-                
-            </div>
-             
-
+             </div> */}
         </div>
     )
 }
-const Events = styled.div`
-    width: 190px;
-    margin: 10px 0px;
-    .h2 {
-      margin: 5px 0px;
-      padding: 10px 0px;
-    }
-    .h5 {
-      padding: 5px 0px;
-    }
-    &:hover {
-        text-decoration: underline;
-      }
-`
