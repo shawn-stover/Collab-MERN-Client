7ab71f159ea96d80a76cb72d6f1389aef0632dbe
Ellie-Lissner <eswalker0723@gmail.com>
2021-07-13 17:43:25 -0500

    committing to pull

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -60,7 +60,7 @@ export default function Profile(props) {
 
             <h2>Welcome, {props.currentUser.name}👋</h2>
             {/* <Link to="/calendar">Calendar</Link> */}
-            {/* <h5>your email is {props.currentUser.email}</h5>
+            {/* <h5>your email is {props.currentUser.email}</h5> */}
 
             <Events>
                 <Link to="/calendar">Calendar</Link>
