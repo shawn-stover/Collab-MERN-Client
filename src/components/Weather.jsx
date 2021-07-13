df8ece3677f8677c153579307c1953953780efaf
Ellie-Lissner <eswalker0723@gmail.com>
2021-07-13 12:00:15 -0500

    some styling added

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default function Weather() {
     const [weather, setWeather] = useState()
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=${process.env.REACT_APP_WEATHER_API}`
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&appid=${process.env.REACT_APP_WEATHER_API}`
     
       const getWeather = async () => {
          try{
@@ -30,9 +30,11 @@ export default function Weather() {
      
       return (
         <div >
-            <h1>Weather</h1>
             <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
-            <p>{weather.weather[0].main}<br/>{weather.main.temp}<br/>{weather.main.temp_min}<br />{weather.main.temp_max}</p>
+            <h3>{weather.weather[0].main}</h3>
+            <h4>Current Temp</h4>
+            <h4>{weather.main.temp}°F</h4>
+            <h5>{weather.main.temp_max}/{weather.main.temp_min}</h5>  
         </div>
       );
 }
