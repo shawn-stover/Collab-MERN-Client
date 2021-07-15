2c4a971784c093aebdc3b9bd5b998e36ecf9e907
nesbita <ariananesbit@gmail.com>
2021-07-15 11:14:13 -0600

    registration done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -151,10 +151,10 @@ export default function Login(props) {
                 />
               </Grid>
               <Grid item xs={12}>
-                <FormControlLabel
+                {/* <FormControlLabel
                   control={<Checkbox value="allowExtraEmails" color="primary" />}
                   label="I want to receive inspiration, marketing promotions and updates via email."
-                />
+                /> */}
               </Grid>
             </Grid>
             <Button
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,45 @@
+import React from 'react';
 import { useState } from "react"
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
 import { Redirect } from "react-router-dom"
 import Profile from './Profile'
+import Title from './Title'
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 export default function Register(props) {
+    const classes = useStyles();
     //state for the controlled form
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
@@ -48,17 +83,102 @@ export default function Register(props) {
     }
 
     //redirect if the user is logged in
-    if(props.currentUser) return <Redirect to="/profile" componenet= {Profile} currentUser={props.currentUser}/>
+    if(props.currentUser) return <Redirect to="/profile" component= {Profile} currentUser={props.currentUser}/>
     return(
-        <div>
-           <h3>Registration Form</h3>
-
+        <div className ='container'>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+            <Title />
+          <Typography component="h1" variant="h5">
+            Enter Your Information Below to Sign Up!
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit} >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField 
+                    id='email-input' 
+                    type='email' 
+                    placeholder="user@domain.com"
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField 
+                  id="password-input"
+                  type="password"
+                  placeholder='password'
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up!
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+      
             <p>{message}</p>
 
-           <form onSubmit= {handleSubmit}>
+         <form onSubmit= {handleSubmit}>
 
-               <label htmlFor='name-input'>Name:</label>
-               <input
+              {/* <label htmlFor='name-input'>Name:</label>
+               <input   
                     id='name-input'
                     type='text'
                     placeholder="enter your name user ..."
@@ -86,10 +206,10 @@ export default function Register(props) {
 
                 <input 
                     type='submit'
-                    value='make new account'
+                    value='make new account' */}
 
-                />
+                {/* /> */}
            </form>
-        </div>
+    </div>
     )
 }
